9d607fae1db7f625290f3424bbd04156e9b402e3
david yuri <davidyuri58@gmail.com>
2026-05-04 16:15:49 -0300

    1.0

--- lista1.py
+++ lista1.py
@@ -1,1 +1,16 @@
-print ("hellow world");
+
+#1. Calculadora Simples
+
+print ("#1. Calculadora Simples")
+
+valor_1=float(input("Digite o primeiro número: "));
+valor_2=float(input("Digite o segundo número: "));
+
+print(f"soma: {valor_1 + valor_2}" )
+print(f"subtração:{valor_1 - valor_2}")
+print(f"multiplicação: {valor_1 * valor_2}")
+print(f"Divisão: {valor_1 / valor_2}")
+
+
+#2. Conversor de Temperatura: 
+Temp=float(input("Digite o primeiro número: "));
